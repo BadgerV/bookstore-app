8df5f6f6beb89308adf1c645a62b52b4391785ba
Segunmaru Faozan <segunfaozan@gmail.com>
2024-03-07 15:32:14 +0100

    added the header component to the frontend

--- bookstore-frontend/src/app/app.routes.ts
+++ bookstore-frontend/src/app/app.routes.ts
@@ -1,3 +1,43 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AddBookComponent } from './pages/add-book/add-book.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { AuthorComponent } from './pages/author/author.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "",
+        redirectTo : "/home",
+        pathMatch : 'full'
+    },
+    {
+        path : "home",
+        component : HomeComponent
+    },
+    {
+        path : "login",
+        component : LoginComponent
+    },
+    {
+        path : "add-book",
+        component : AddBookComponent
+    },
+    {
+        path : "search",
+        component : SearchComponent
+    },
+    {
+        path : "signup",
+        component : SignupComponent
+    },
+    {
+        path : "author-page",
+        component : AuthorComponent
+    }, {
+        path : "*",
+        component : PageNotFoundComponent
+    }
+];
